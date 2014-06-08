af0ac886465b9ef421fd9d9ce432ec838107fa13
Chris Hanks <christopher.m.hanks@gmail.com>
2014-06-08 15:27:06 -0700

    Ensure that schemas besides public are picked up.

--- spec/schema_spec.rb
+++ spec/schema_spec.rb
@@ -11,4 +11,14 @@ describe PGExaminer do
     schema.name.should == 'public'
     schema.tables.should == []
   end
+
+  it "should detect other user-added schemas" do
+    execute <<-SQL
+      CREATE SCHEMA my_schema;
+    SQL
+
+    result = PGExaminer.examine(CONNECTION)
+    result.schemas.length.should == 2
+    result.schemas.map(&:name).should == %w(public my_schema)
+  end
 end
