aa27a27810d7677e5b4eadac58d228ff692f045c
Chris Hanks <christopher.m.hanks@gmail.com>
2014-06-11 16:52:29 -0700

    Make sure the constraint comparisons cover foreign keys.

--- spec/constraint_spec.rb
+++ spec/constraint_spec.rb
@@ -51,4 +51,72 @@ describe PGExaminer do
     a.should_not == f
     e.should_not == f
   end
+
+  it "should consider foreign keys when differentiating between schemas" do
+    a = examine <<-SQL
+      CREATE TABLE parent (
+        int1 integer PRIMARY KEY,
+        int2 integer UNIQUE
+      );
+
+      CREATE TABLE child (
+        parent_id integer REFERENCES parent
+      );
+    SQL
+
+    b = examine <<-SQL
+      CREATE TABLE parent (
+        int1 integer PRIMARY KEY,
+        int2 integer UNIQUE
+      );
+
+      CREATE TABLE child (
+        parent_id integer,
+        FOREIGN KEY (parent_id) REFERENCES parent
+      );
+    SQL
+
+    c = examine <<-SQL
+      CREATE TABLE parent (
+        int1 integer PRIMARY KEY,
+        int2 integer UNIQUE
+      );
+
+      CREATE TABLE child (
+        parent_id integer,
+        FOREIGN KEY (parent_id) REFERENCES parent (int2)
+      );
+    SQL
+
+    d = examine <<-SQL
+      CREATE TABLE parent (
+        int1 integer PRIMARY KEY,
+        int2 integer UNIQUE
+      );
+
+      CREATE TABLE child (
+        parent_id integer,
+        FOREIGN KEY (parent_id) REFERENCES parent ON UPDATE CASCADE
+      );
+    SQL
+
+    e = examine <<-SQL
+      CREATE TABLE parent (
+        int1 integer PRIMARY KEY,
+        int2 integer UNIQUE
+      );
+
+      CREATE TABLE child (
+        parent_id integer,
+        FOREIGN KEY (parent_id) REFERENCES parent ON DELETE CASCADE
+      );
+    SQL
+
+    a.should == b
+    a.should_not == c
+    b.should_not == c
+    b.should_not == d
+    b.should_not == e
+    d.should_not == e
+  end
 end
